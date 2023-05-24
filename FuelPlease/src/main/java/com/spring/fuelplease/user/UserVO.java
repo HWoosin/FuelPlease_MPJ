e67ca1f3de7d2c67896ef85cca6ac953482a0239
woosin <ws1340cc@naver.com>
2023-05-24 17:30:56 +0900

    VO, mapper, service, controller 파일 정리230524

--- FuelPlease/src/main/java/com/spring/fuelplease/user/UserVO.java
+++
@@ -1,5 +0,0 @@
-package com.spring.fuelplease.user;
-
-public class UserVO {
-
-}
---
+++ FuelPlease/src/main/java/com/spring/fuelplease/user/service/IUserService.java
@@ -0,0 +1,17 @@
+package com.spring.fuelplease.user.service;
+
+import com.spring.fuelplease.voCenter.UserVO;
+
+public interface IUserService {
+
+	//회원정보 얻어오기
+	
+	//로그인
+		
+	//회원가입
+	void userjoin(UserVO vo);
+		
+	//아이디 중복확인
+		
+	//회원정보 수정
+}
